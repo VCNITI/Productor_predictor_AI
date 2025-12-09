ef56f7aaad802466e9ff329382b3904c3351609b
VCNITI <hellovcniti@gmail.com>
2025-12-09 16:29:39 +0530

    Remove VcnitiOverview component and its references

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,34 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import AIPlanner from '../components/AIPlanner';
-import PredictorForm from '../components/PredictorForm';
-import AboutAIPlanner from '../components/AboutAIPlanner';
 import Hero from '../components/Hero';
 
+
 const HomePage: React.FC = () => {
-  const [showPredictorForm, setShowPredictorForm] = useState(false);
-
-  const handleUsePlannerClick = () => {
-    setShowPredictorForm(true);
-  };
-
-  const handleHomeClick = () => {
-    setShowPredictorForm(false);
-  };
-
   return (
     <div>
-      <Header onHomeClick={handleHomeClick} />
+      <Header />
       <Hero />
-      {showPredictorForm ? (
-        <>
-          <AboutAIPlanner />
-          <PredictorForm />
-        </>
-      ) : (
-        <AIPlanner onUsePlannerClick={handleUsePlannerClick} />
-      )}
+
+      <AIPlanner />
       <Footer />
     </div>
   );
